b7443afc52ee43f922acc96864752db5039902d8
sakshi59 <74191300+sakshi59@users.noreply.github.com>
2024-11-13 12:41:08 -0800

    engine.py

--- engine.py
+++ engine.py
@@ -1,13 +1,13 @@
 class Value:
-    """ stores a single scalar value and its gradient """
+    
 
     def __init__(self, data, _children=(), _op=''):
         self.data = data
         self.grad = 0
-        # internal variables used for autograd graph construction
+        
         self._backward = lambda: None
         self._prev = set(_children)
-        self._op = _op # the op that produced this node, for graphviz / debugging / etc
+        self._op = _op 
 
     def __repr__(self):
         return f"Value(data={self.data}, grad={self.grad})"
@@ -55,7 +55,7 @@ class Value:
     
     def backward(self):
 
-        # topological order all of the children in the graph
+        
         topo = []
         visited = set()
         def build_topo(v):
@@ -66,30 +66,30 @@ class Value:
                 topo.append(v)
         build_topo(self)
 
-        # go one variable at a time and apply the chain rule to get its gradient
+        
         self.grad = 1
         for v in reversed(topo):
             v._backward()
     
-    def __sub__(self, other): # self - other
+    def __sub__(self, other): 
         return self + (-other)
 
-    def __rsub__(self, other): # other - self
+    def __rsub__(self, other): 
         return other + (-self)
 
-    def __rmul__(self, other): # other * self
+    def __rmul__(self, other): 
         return self * other
 
-    def __neg__(self): # -self
+    def __neg__(self): 
         return self * -1
 
-    def __radd__(self, other): # other + self
+    def __radd__(self, other):
         return self + other
     
-    def __truediv__(self, other): # self / other
+    def __truediv__(self, other): 
         return self * other**-1
 
-    def __rtruediv__(self, other): # other / self
+    def __rtruediv__(self, other): 
         return other * self**-1
 
     def __repr__(self):
